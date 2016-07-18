d6cd668ee71e5383c1da2f04a130ef889b3e5765
Yung Chun Ern Martin <martinyce@gmial.com>
2016-07-18 15:57:41 +0800

    done

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,9 +1,15 @@
 require 'byebug'
+
 get '/' do
   erb :"static/index"
 end
 
 post '/urls' do
-	Url.create(long_url: params["long_url"], shortened_url: "www.bit.url/#{Url.shorten}")
+	Url.create(long_url: params["long_url"], shortened_url: Url.shorten)
 	redirect '/'
+end
+
+get '/:shortened_url' do
+	ori_url = Url.find_by(shortened_url: params['shortened_url']).long_url
+	redirect 'https://' + ori_url
 end
